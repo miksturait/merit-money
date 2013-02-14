219e8b9df0cdb52be5ccf785e17a1f711a48370a
Michal Czyz <michalczy@gmail.com>
2013-02-14 20:15:20 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sks::Application.config.secret_token = '930edbde54002f331f07b0e35a941aec905f59356bc8cdfe2db2543898115120a175b76e0625237931f65609e18b78320b737c0938a2339224e5f3183ffab647'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sks::Application.config.session_store :cookie_store, key: '_sks_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sks::Application.config.session_store :active_record_store
