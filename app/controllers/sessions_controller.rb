3e3b3f30be21789e875b525ebc67c39dd98911bc
Michal Czyz <michalczy@gmail.com>
2013-02-14 20:16:32 +0100

    rails_apps_composer: controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+  def new
+    redirect_to '/auth/google_oauth2'
+  end
+
+
+  def create
+    auth = request.env["omniauth.auth"]
+    user = User.where(:provider => auth['provider'], 
+                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    if user.email.blank?
+      redirect_to edit_user_path(user), :alert => "Please enter your email address."
+    else
+      redirect_to root_url, :notice => 'Signed in!'
+    end
+
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url, :notice => 'Signed out!'
+  end
+
+  def failure
+    redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
+  end
+
+end
