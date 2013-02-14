92c9ee0795839514304f97d44e5957c0f58f1964
Michal Czyz <michalczy@gmail.com>
2013-02-14 20:16:32 +0100

    rails_apps_composer: models

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  attr_accessible :provider, :uid, :name, :email
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+         user.name = auth['info']['name'] || ""
+         user.email = auth['info']['email'] || ""
+      end
+    end
+  end
+
+end
