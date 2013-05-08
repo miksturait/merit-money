a6c900045b95ae09623c38a965cc09712e451129
Michal Czyz <michalczy@gmail.com>
2013-05-08 08:08:26 +0200

    some fix ;-)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Sks::Application.config.session_store :cookie_store, key: '_sks_session'
+Sks::Application.config.session_store :cookie_store, key: '_sks_s'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
