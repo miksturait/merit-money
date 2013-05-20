8bd8c1bf002c3c4dfb9f4ecd5a56dd13fd4b4925
Michal Czyz <michalczy@gmail.com>
2013-05-20 21:36:36 +0200

    fix seed loading

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) are set in the file config/application.yml.
 # See http://railsapps.github.com/rails-environment-variables.html
+unless Rails.env.production?
+  ::AllowedUser.delete_all
+  ::User.delete_all
+end
 
 users_raw = "Tomasz Bąk <t.bak@selleo.com>, Tomasz Borowski <t.borowski@selleo.com>, Rafał Bromirski <rafal.bromirski@gmail.com>, Tomasz Czana <tomasz.czana@gmail.com>, Michał Czyż <michalczyz@gmail.com>, Bartlomiej Danek <bartek.danek@gmail.com>, Sebastian Ewak <sebastian.ewak@gmail.com>, Arek Janik <arek.mp@gmail.com>, Radosław Jędryszczak <socjopata@gmail.com>, Szymon Kieloch <skieloch@gmail.com>, Blazej Kosmowski <blazejek@gmail.com>, Arkadiusz Kwaśny <arkadiusz.kwasny@gmail.com>, Tomasz Noworyta <tomasz.noworyta@gmail.com>, Adrian Osowski <aossowski@gmail.com>, Grzegorz Rduch <grduch@gmail.com>, Ireneusz Skrobis <irek.skrobis@gmail.com>, Bartłomiej Wójtowicz <wojtowicz.bartlomiej@gmail.com>, Wojciech Ryrych <wojtek.ryrych@gmail.com>, Dariusz Wylon <pract.pl@gmail.com>"
 users_raw.split(', ').each do |user_info|
@@ -17,8 +21,6 @@ end
 
 if Rails.env.development?
   AllowedUser.all.each do |au|
-    #unless %w(michalczyz@gmail.com wojtek.ryrych@gmail.com).include?(au.email)
-      User.create(name: au.name, email: au.email)
-    #end
+    User.create(name: au.name, email: au.email)
   end
 end
