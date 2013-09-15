e188d83fe9f58566a0bc050e3611c2f4c185e5c1
Michal Czyz <michalczyz@gmail.com>
2013-09-15 13:10:52 +0200

    heroku ... ech

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Sks::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( active_admin.css active_admin.js)
+  config.assets.precompile += %w( active_admin.css active_admin.js active_admin/print.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
