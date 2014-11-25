20ebaa2536dab49bbed9d776fa6b587294491772
Michal Czyz <michalczyz@gmail.com>
2014-11-25 08:15:35 +0100

    for demo purpose let's open it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    if AllowedUser.where(email: auth['info']['email']).exists?
+    # if AllowedUser.where(email: auth['info']['email']).exists?
       user = User.where(:provider => auth['provider'],
                         :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
       session[:user_id] = user.id
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
       end
     else
       redirect_to about_url, :alert => "Authentication error: sorry your email is not in the list"
-    end
+    # end
   end
 
   def destroy
